d9726fa3d0f38cf25362c61909e879a0a87553bc
Evangelos Batsalis <vbatsalis@gmail.com>
2024-01-10 23:04:04 +0200

    first init

---
+++ src/main/java/gr/vbatsalis/SplitPayment/SplitPaymentApplication.java
@@ -0,0 +1,13 @@
+package gr.vbatsalis.SplitPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/vbatsalis/SplitPayment/SplitPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.vbatsalis.SplitPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
