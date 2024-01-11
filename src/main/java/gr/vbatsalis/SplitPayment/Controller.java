8f7d063d302925257b1286d66daa8efab00028ec
Evangelos Batsalis <vbatsalis@gmail.com>
2024-01-12 01:07:50 +0200

    changes repo

--- src/main/java/gr/vbatsalis/SplitPayment/Controller.java
+++ src/main/java/gr/vbatsalis/SplitPayment/Controller.java
@@ -2,6 +2,7 @@ package gr.vbatsalis.SplitPayment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,11 +17,17 @@ public class Controller {
         this.userSevice = userSevice;
     }
 
+//    @GetMapping("/get/{id}")
+//    public User getUser(@PathVariable int id){
+//        System.out.println("ok");
+//        return userSevice.getUserById(id);
+//    }
     @GetMapping("/get/{id}")
-    public User getUser(@PathVariable int id){
-        return userSevice.getUserById(id);
+    public String getUserById(@PathVariable int id, Model model){
+        User user = userSevice.getUserById(id);
+        model.addAttribute("user", user);
+        return dataView;
     }
-
     @PostMapping("/new")
     public void saveUser(@RequestBody User user){
         userSevice.saveUser(user);
--- src/main/java/gr/vbatsalis/SplitPayment/UserSevice.java
+++ src/main/java/gr/vbatsalis/SplitPayment/UserSevice.java
@@ -4,6 +4,8 @@ import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class UserSevice {
